218299171e1ae89d794e6151e7b13645ea92bf10
melbaa <none@no-reply>
2023-02-08 15:50:28 +0200

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# SmartHealer
+*for World of Warcraft 1.12.1 (Vanilla)*
+
+Autoscales heals in macros (/heal <spell_name>) and click heals for pfUI, Clique and ClassicMouseover.
+
+Scaling is done using HealComm-1.0 library (part of the package) or [TheoryCraft](https://wow.curseforge.com/projects/project-1644) (if present).  
+Addon checks missing HPs of target (or player if self cast), compares it with calculated healing done and selects the lowest rank needed to fully heal the target. If TheoryCraft is installed, addon also checks if you have enough mana to cast spell of selected rank. If you do not, it will try to use highest possible rank for which you have enough mana.
+
+## Commands:
+- `/heal <spell_name>[, overheal_multiplier]`  
+  Used in macros to cast optimal rank of heal.  
+  Overheal multiplier is optional and should be separated from spell name by comma "," or semicolon ";". It will override dafault overheal multiplier. See the next command.  
+
+- `/sh_overheal <multiplier>`  
+  Sets new default overheal multiplier. When selecting spell rank, calculated heal must be higher then missing HP * multiplier. Valid multiplier is number or percentage (1.15 or 115%).   
+  If used without argument, prints current overheal multiplier.  
+
+*NOTE:*  
+Spell name shouldn't contain rank. If there is a rank, heal won't be scaled. It means that `/heal Healing Wave` will use rank as needed, but `/heal Healing Wave(Rank 3)` will always cast rank 3.  
+The same goes for supported addons. Do not specify rank in click actions (in case of Clique, spell must be set to "Max").  
+
+## Installation
+1. Download **[Latest Version](https://gitlab.com/AMGarkin/SmartHealer/-/archive/master/SmartHealer-master.zip)**
+2. Unpack the Zip file
+3. Rename the folder "SmartHealer-master" to "SmartHealer"
+4. Copy "SmartHealer" into \<WoW-directory\>\Interface\AddOns
+5. Restart WoW
+
+### Credits:
+Garkin's repo https://gitlab.com/AMGarkin/SmartHealer  
+Original idea of this addon is based on Ogrisch's [LazySpell](https://github.com/satan666/LazySpell).
---
+++ SmartHealer.lua
@@ -0,0 +1,275 @@
+--Original idea of this addon is based on Ogrisch's LazySpell
+
+SmartHealer = AceLibrary("AceAddon-2.0"):new("AceHook-2.1", "AceConsole-2.0", "AceDB-2.0")
+SmartHealer:RegisterDB("SmartHealerDB")
+SmartHealer:RegisterDefaults("account", { overheal = 1 })
+
+local libHC = AceLibrary("HealComm-1.0")
+local libIB = AceLibrary("ItemBonusLib-1.0")
+local libSC = AceLibrary("SpellCache-1.0")
+
+function SmartHealer:OnEnable()
+	if Clique and Clique.CastSpell then
+		self:Hook(Clique, "CastSpell", "Clique_CastSpell")
+	end
+
+	if CM and CM.CastSpell then
+		self:Hook(CM, "CastSpell", "CM_CastSpell")
+	end
+
+	if pfUI and pfUI.uf and pfUI.uf.ClickAction then
+		self:Hook(pfUI.uf, "ClickAction", "pfUI_ClickAction")
+	end
+	
+	self:RegisterChatCommand({ "/heal" }, function(arg) SmartHealer:CastHeal(arg) end, "SMARTHEALER")
+	self:RegisterChatCommand({ "/sh_overheal" }, function(arg) SmartHealer:Overheal(arg) end, "SMARTOVERHEALER")
+    self:Print('loaded')
+end
+
+-------------------------------------------------------------------------------
+-- Handler function for /heal <spell_name>[, overheal_multiplier]
+-------------------------------------------------------------------------------
+-- Function automatically choose which rank of heal will be casted based on
+-- amount of missing life.
+--
+-- NOTE: Argument "spellName" should be always heal and shouldn't contain rank.
+-- If there is a rank, function won't scale it. It means that "Healing Wave"
+-- will use rank as needed, but "Healing Wave(Rank 3)" will always cast rank 3.
+-- Argument "spellName" can contain overheal multiplier information separated
+-- by "," or ";" and it should be either number (1.1) or percentage (110%).
+--
+-- Examples:
+-- SamrtHealer:CastSpell("Healing Wave")			--/heal Healing Wave
+-- SamrtHealer:CastSpell("Healing Wave, 1.15")		--/heal Healing Wave, 1.15
+-- SamrtHealer:CastSpell("Healing Wave;120%")		--/heal Healing Wave;120%
+-------------------------------------------------------------------------------
+function SmartHealer:CastHeal(spellName)
+	local overheal
+
+	if not spellName or type(spellName) ~= "string" then
+		return
+	else
+		spellName = string.gsub(spellName, "^%s*(.-)%s*$", "%1")	--strip leading and trailing space characters
+		spellName = string.gsub(spellName, "%s+", " ")				--replace all space character with actual space
+
+		local _, _, arg = string.find(spellName, "[,;]%s*(.-)$")	--tries to find overheal multiplier (number after spell name, separated by "," or ";")
+		if arg then
+			local _, _, percent = string.find(arg, "(%d+)%%")
+			if percent then
+				overheal = tonumber(percent) / 100
+			else
+				overheal = tonumber(arg)
+			end
+
+			spellName = string.gsub(spellName, "[,;].*", "")		--removes everything after first "," or ";"
+		end
+
+		if not overheal then
+			overheal = self.db.account.overheal
+		end
+	end
+
+	local spell, rank = libSC:GetRanklessSpellName(spellName)
+	local unit, onSelf
+
+	if IsAltKeyDown() then
+		unit = "player"
+		onSelf = true
+	elseif UnitExists("target") and UnitCanAssist("player", "target") then
+		unit = "target"
+	end
+	
+	if unit == nil then
+		if GetCVar("autoSelfCast") == "1" then
+			unit = "player"
+			onSelf = true
+		else
+			return
+		end
+	end
+	
+	if spell and rank == nil and libHC.Spells[spell] then
+		rank = self:GetOptimalRank(spell, unit, overheal)
+		if rank then
+			spellName = libSC:GetSpellNameText(spell, rank)
+		end
+	end	
+	
+	CastSpellByName(spellName, onSelf)
+
+	if UnitIsUnit("player", unit) then
+		if SpellIsTargeting() then
+			SpellTargetUnit(unit)
+		end
+		if SpellIsTargeting() then
+			SpellStopTargeting()
+		end
+	end
+end
+
+-------------------------------------------------------------------------------
+-- Handler function for /sh_overheal
+-------------------------------------------------------------------------------
+-- Saves new default overheal multiplier, argument "value" should be either
+-- string or number. String could contain number ("1.15") or percentage ("115%")
+-- If "value" is not specified or invalid, function prints current overheal
+-- multiplier.
+-------------------------------------------------------------------------------
+function SmartHealer:Overheal(value)
+	if value and type(value) == "string" then
+		value = string.gsub(value, "^%s*(.-)%s*$", "%1")
+
+		local _, _, percent = string.find(value, "(%d+)%%")
+		if percent then
+			value = tonumber(percent) / 100
+		else
+			value = tonumber(value)
+		end
+	end
+
+	if type(value) == "number" then
+		self.db.account.overheal = math.floor(value * 1000 + 0.5) / 1000
+	else
+		self:Print("Overheal multiplier: ", self.db.account.overheal, "(", self.db.account.overheal * 100, "%)")
+	end
+end
+
+-------------------------------------------------------------------------------
+-- Function selects optimal spell rank to cast based on unit's missing HP
+-------------------------------------------------------------------------------
+-- spell	- spell name to cast ("Healing Wave")
+-- unit	 	- unitId ("player", "target", ...)
+-- overheal	- overheal multiplier. If nil, then using self.db.account.overheal.
+-------------------------------------------------------------------------------
+function SmartHealer:GetOptimalRank(spell, unit, overheal)
+	if not libSC.data[spell] then
+        self:Print('smartheal rank not found')
+        return
+    end
+
+	local bonus, power, mod
+	if TheoryCraft == nil then
+		bonus = tonumber(libIB:GetBonus("HEAL"))
+		power, mod = libHC:GetUnitSpellPower(unit, spell)
+		local buffpower, buffmod = libHC:GetBuffSpellPower()
+		bonus = bonus + buffpower
+		mod = mod * buffmod
+	end
+	local missing = UnitHealthMax(unit) - UnitHealth(unit)
+	local max_rank = tonumber(libSC.data[spell].Rank)
+	local rank = max_rank
+	overheal = overheal or self.db.account.overheal
+
+	local mana = UnitMana("player")
+    local spelldata = nil
+	for i = max_rank, 1, -1 do
+		spellData = TheoryCraft ~= nil and TheoryCraft_GetSpellDataByName(spell, i)
+		if spellData then
+    			if mana >= spellData.manacost then
+				if spellData.averagehealnocrit > (missing * overheal) then
+					rank = i
+				else
+					break
+				end
+			else
+				rank = i > 1 and i - 1 or 1
+			end
+		else
+			local heal = (libHC.Spells[spell][i](bonus) + power) * mod
+			if heal > (missing * overheal) then
+				rank = i
+			else
+				break
+			end
+		end
+	end
+    --[[
+    self:Print(spell
+            .. ' rank ' .. rank
+            .. ' hp ' .. math.floor(spellData.averagehealnocrit)
+            .. ' hpm ' .. (spellData.averagehealnocrit / spellData.manacost)
+            .. ' mana ' .. spellData.manacost )
+    ]]
+	return rank
+end
+
+-------------------------------------------------------------------------------
+-- Support for Clique
+-------------------------------------------------------------------------------
+function SmartHealer:Clique_CastSpell(clique, spellName, unit)
+	unit = unit or clique.unit
+
+	if UnitExists(unit) then
+		local spell, rank = libSC:GetRanklessSpellName(spellName)
+
+		if spell and rank == nil and libHC.Spells[spell] then
+			rank = self:GetOptimalRank(spellName, unit)
+			if rank then
+				spellName = libSC:GetSpellNameText(spell, rank)
+			end
+		end
+	end
+
+	self.hooks[Clique]["CastSpell"](clique, spellName, unit)
+end
+
+-------------------------------------------------------------------------------
+-- Support for ClassicMouseover
+-------------------------------------------------------------------------------
+function SmartHealer:CM_CastSpell(cm, spellName, unit)
+	if UnitExists(unit) then
+		local spell, rank = libSC:GetRanklessSpellName(spellName)
+
+		if spell and rank == nil and libHC.Spells[spell] then
+			rank = self:GetOptimalRank(spellName, unit)
+			if rank then
+				spellName = libSC:GetSpellNameText(spell, rank)
+			end
+		end
+	end
+
+	self.hooks[CM]["CastSpell"](cm, spellName, unit)
+end
+
+-------------------------------------------------------------------------------
+-- Support for pfUI
+-------------------------------------------------------------------------------
+function SmartHealer:pfUI_ClickAction(pfui_uf, button)
+	local spellName = ""
+	local key = "clickcast"
+
+	if button == "LeftButton" then
+		local unit = (this.label or "")..(this.id or "")
+
+		if UnitExists(unit) then
+			if this.config.clickcast == "1" then
+				if IsShiftKeyDown() then
+					key = key.."_shift"
+				elseif IsAltKeyDown() then
+					key = key.."_alt"
+				elseif IsControlKeyDown() then
+					key = key.."_ctrl"
+				end
+
+				spellName = pfUI_config.unitframes[key]
+
+				if spellName ~= "" then
+					local spell, rank = libSC:GetRanklessSpellName(spellName)
+
+					if spell and rank == nil and libHC.Spells[spell] then
+						rank = self:GetOptimalRank(spellName, unit)
+						if rank then
+							pfUI_config.unitframes[key] = libSC:GetSpellNameText(spell, rank)
+						end
+					end
+				end
+			end
+		end
+	end
+
+	self.hooks[pfUI.uf]["ClickAction"](pfui_uf, button)
+
+	if spellName ~= "" then
+		pfUI_config.unitframes[key] = spellName
+	end
+end
